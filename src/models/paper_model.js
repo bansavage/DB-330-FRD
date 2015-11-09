4511f31b2858cbcebc6a67a22162010bf28563d7
Chris Dumlao <cxd3498@rit.edu>
2015-11-09 02:54:25 -0500

    Paper Model Changes
    Resolved caching module caching problems

--- src/models/paper_model.js
+++ src/models/paper_model.js
@@ -1,38 +1,56 @@
-//variables are not global, on availible in the scope of this module
-var paper_id, paper_title, paper_abstract, paper_citation;
+/*Needs to be this way so he can have multiple instances
+  of a paper object
+*/
+module.exports = function( paper_info ){
 
-/*                    *\
-**  Setter Functions  **
-\*                    */
-
-//Exports allows other modules to use these functions
-exports.setPaperId = function ( id_number ){
-	paper_id = id_number;
-};
-
-exports.setPaperTitle = function ( title ){
-	paper_title = title;
-};
-
-exports.setPaperAbstract = function ( abstract ){
-	paper_abstract = abstract;
-};
-
-exports.setPaperCitation = function ( citation ){
-	paper_citation = citation;
-};
-
-/*                    *\
-**  Getter Functions  **
-\*                    */
-exports.getPaperInfo = function(){
-
-	//returns literal object with the values of this module
-	return{
-		paper_id: paper_id,
-		paper_title: paper_title,
-		paper_abstract: paper_abstract,
-		paper_citation: paper_citation
+	var values = {
+		paper_id: null,
+		paper_title: null,
+		paper_abstract: null,
+		paper_citation: null
 	};
 
+	//Sets propety for paper if exists
+	for( var property in values ){
+		if( values[property] !== 'undefined' ){
+			values[property] = paper_info[property];
+		}
+	}
+
+	var paper_functions = {
+		
+		//returns array of 
+		getPaperInfo: function(){
+			return values;
+		},
+
+		/*                    *\
+		**  Setter Functions  **
+		\*                    */
+
+		setPaperId: function ( id_number ){
+			values['paper_id'] = id_number;
+		},
+
+		setPaperTitle: function ( title ){
+			values['paper_title'] = title;
+		},
+
+		setPaperAbstract: function ( abstract ){
+			values['paper_abstract'] = abstract;
+		},
+
+		setPaperCitation: function ( citation ){
+			values['paper_citation'] = id_number;
+		}
+
+
+	};
+
+	//returns above functions
+	//an array of functions for this module
+	return paper_functions;
+
 };
+
+
