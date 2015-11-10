080d974d00ff3d2bfdbf80e96ad313fdbde2e0e2
chris dumlao <cxd3498@rit.edu>
2015-11-10 17:32:07 -0500

    Paper Model Update

--- src/models/paper_model.js
+++ src/models/paper_model.js
@@ -1,31 +1,143 @@
-/*Needs to be this way so he can have multiple instances
-  of a paper object
-*/
+var mysql = require('mysql');
+var db = require('../database/db_pool');
+var uuid = require('node-uuid');
+
 var Paper = function(){
 
-	this.data = {
-		paper_id: null,
-		paper_title: null,
-		paper_abstract: null,
-		paper_citation: null
-	};
+	/No need for Getters and Setters
 
-	//sets properties of a paper if exists
-	this.fill = function( paper_info ){
+    // If an id is given, find it in the database, use those attributes
+    if (attrs.p_id !== undefined){
+      this.data = {
+        paper_id : attrs.p_id,
+        paper_title: "",
+        paper_abstract: "",
+        paper_citation: ""
+      };
+      console.log(`fetching`);
+      //fetch
+    }else{
+      this.data = {
+        paper_id: uuid.v1(),
+        paper_title: "",
+        paper_abstract: "",
+        paper_citation : ""
+      };
 
-		for( var property in this.data ){
-			if( this.data[property] !== 'undefined' ){
-				this.data[property] = paper_info[property];
-			}
-		}
-		
-	};
+      for(var property in this.data ){
+        console.log(attrs[property]);
+  			if(attrs[property] !== undefined){
+  				this.data[property] = attrs[property];
+  			}
+  		}
+    }
 
-	//returns array of a papers properties
-	this.getPaperInfo = function(){
-		return this.data;
-	},
+    //Updates the properties in the database
+    //Takes an err and an array of properties to update
+    //We never need to create a property because default values are used
+    //For the properties not initially set
+    this.updateProps = function(err, callback){
+      //Example Connection
+      var self = this;
+      db.getConnection(function(err, connection) {
+        if (err) {throw err;}
+        // Use the connection
+        var sql = "SELECT * FROM frd.papers where paper_id = ?";
+        var inserts = [self.p_id];                             //Problem: need to find way to get self into this callback method
+        sql = mysql.format(sql, inserts);
 
+        connection.query(sql, function(err, rows) {
+          if (err) {throw err;}
+          // And done with the connection.
+          //Set Rows Here
+          callback(rows);
+          self.data = rows;
+          connection.release();
+          // Don't use the connection here, it has been returned to the pool.
+        });
+      });
+      //If something bad happens add a string to err
+      //Example: err = "update could not finish"
+      return;
+    };
+
+    //Deletes the properties
+    //Takes an err and an array of properties to delete
+    this.deleteProps = function(err, callback){
+      //Example Connection
+      var self = this;
+      db.getConnection(function(err, connection) {
+        if (err) {throw err;}
+        // Use the connection
+        var sql = "SELECT * FROM frd.papers where paper_id = ?";
+        var inserts = [self.p_id];                             //Problem: need to find way to get self into this callback method
+        sql = mysql.format(sql, inserts);
+
+        connection.query(sql, function(err, rows) {
+          if (err) {throw err;}
+          // And done with the connection.
+          //Set Rows Here
+          callback(rows);
+          self.data = rows;
+          connection.release();
+          // Don't use the connection here, it has been returned to the pool.
+        });
+      });
+      //If something bad happens add a string to err
+      //Example: err = "update could not finish"
+      return;
+    };
+
+    //Fetches all properties from the database using p_id
+    //qdata is an empty array
+    this.fetchProps = function(err, callback){
+      //Example Connection
+
+        var self = this;
+        db.getConnection(function(err, connection) {
+          try{
+            if (err) {throw err;}
+            // Use the connection
+            var sql = "SELECT ??,??,??,?? FROM frd.users where p_id = ?";
+            var inserts = ['paper_id','paper_title','paper_abstract','paper_citation', self.data.p_id];
+            sql = mysql.format(sql, inserts);
+
+
+            connection.query(sql, function(err, rows) {
+              try{
+                if (err) {throw err;}
+                if (rows[0] == undefined) {throw new Error('No row data');}
+                // And done with the connection.
+                //Set Rows Here
+                for (prop in rows[0]){
+                  console.log(`prop ${prop} = ${rows[0][prop]}`);
+                  self.data[prop] = rows[0][prop];
+                }
+
+                //self.data = rows;
+                callback(); // All values have been set
+                connection.release();
+              }catch (err){
+                //Create new object attib if no data is found
+                self.data = {
+                  paper_id: uuid.v1(),
+                  paper_title: "",
+                  paper_abstract: "",
+                  paper_citation: ""
+                };
+              }
+            });
+
+          }catch(err){
+            console.log(`${err}`);
+          }
+            // Don't use the connection here, it has been returned to the pool.
+        });
+      //If something bad happens add a string to err
+      //Example: err = "update could not finish"
+      return;
+    };
+    
 	/*                    *\
 	**  Setter Functions  **
 	\*                    */
@@ -51,10 +163,6 @@ var Paper = function(){
 /*
 **Returns an actual new instance of a paper
 **/
-module.exports = function( paper_info ){
-	var instance = new Paper();
-	instance.fill(paper_info);
-	return instance;
-};
+module.exports = paper_model;
 
 
