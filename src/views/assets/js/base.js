cbfdbe237ecf8043812ea8bd405ef03a2b12ef21
Chris Dumlao <cxd3498@rit.edu>
2015-12-08 08:52:25 -0500

    Views added and placed into appropriate folders

--- src/views/assets/js/base.js
+++ src/views/assets/js/base.js
@@ -1,44 +1,21 @@
+function $(id){
+	return document.getElementById(id);
+}
 
 function init(){
-	var submit = document.getElementById('db-submit');
-	submit.addEventListener('click', submitForm );
+	var loginForm = $("form");
+	loginForm.addEventListener('submit', submitForm );
+	
 }
 
 function submitForm(evt){
 
-	var username = document.getElementById("inputUserName").value;
-	var password = document.getElementById("inputPassword").value;
+	var username = $("inputUserName").value;
+	var password = $("inputPassword").value;
 
 	console.log("username was: " + username);
 	console.log("password was: " + password);
-
-	$.ajax({
-		url: "/api/authenticate",
-		type: "POST",
-		data : {
-			username: username,
-			password: password
-		},
-		success : function(data, textStatus, jqXHR){
-			console.log(data);
-			var token = data.token;
-			//Save to local storage
-			localStorage.setItem("token", token);
-
-			if (data.success){
-				$.ajax({
-						url: "http://localhost:7000/controlpanel",
-						data: {token: token},
-						type: "GET",
-					}
-				);
-				return true;
-			};
-		},
-		error : function(jqXHR, textStatus, errorThrown){
-			console.log(textStatus);
-		}
-	});
-	//evt.preventDefault();
+	evt.preventDefault();
 	return false;
 }
+
