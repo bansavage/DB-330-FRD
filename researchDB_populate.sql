86c764bedc43767c6ac07d4a6e8b4583aef2e7fa
lance13c <pianoman13c@gmail.com>
2015-12-11 03:24:57 -0500

    Added missing papers users maps

--- researchDB_populate.sql
+++ researchDB_populate.sql
@@ -42,37 +42,37 @@ insert into PAPER_KEYWORDS values ('12', 'word', '3','3');
 insert into PAPER_KEYWORDS values ('13', 'word', '4','3');
 
 insert into searchable_keywords values('4', 'science');
-insert into PAPER_KEYWORDS values ('14', 'Biology', '8','4');
-insert into PAPER_KEYWORDS values ('15', 'Biology', '9','4');
-insert into PAPER_KEYWORDS values ('16', 'Biology', '10','4');
-insert into PAPER_KEYWORDS values ('17', 'Physics', '11','4');
-insert into PAPER_KEYWORDS values ('18', 'Physics', '12','4');
-insert into PAPER_KEYWORDS values ('19', 'Physics', '13','4');
-insert into PAPER_KEYWORDS values ('20', 'Chemistry', '14','4');
-insert into PAPER_KEYWORDS values ('21', 'Chemistry', '15','4');
-insert into PAPER_KEYWORDS values ('22', 'Chemistry', '16','4');
-insert into PAPER_KEYWORDS values ('23', 'Chemistry', '17','4');
-insert into PAPER_KEYWORDS values ('24', 'Environment', '18','4');
-insert into PAPER_KEYWORDS values ('25', 'Communication', '19','4');
-insert into PAPER_KEYWORDS values ('26', 'Technology', '20','4');
-insert into PAPER_KEYWORDS values ('27', 'Animal Reintroduction', '21','4');
+insert into PAPER_KEYWORDS values ('14', 'biology', '8','4');
+insert into PAPER_KEYWORDS values ('15', 'biology', '9','4');
+insert into PAPER_KEYWORDS values ('16', 'biology', '10','4');
+insert into PAPER_KEYWORDS values ('17', 'physics', '11','4');
+insert into PAPER_KEYWORDS values ('18', 'physics', '12','4');
+insert into PAPER_KEYWORDS values ('19', 'physics', '13','4');
+insert into PAPER_KEYWORDS values ('20', 'chemistry', '14','4');
+insert into PAPER_KEYWORDS values ('21', 'chemistry', '15','4');
+insert into PAPER_KEYWORDS values ('22', 'chemistry', '16','4');
+insert into PAPER_KEYWORDS values ('23', 'chemistry', '17','4');
+insert into PAPER_KEYWORDS values ('24', 'environment', '18','4');
+insert into PAPER_KEYWORDS values ('25', 'communication', '19','4');
+insert into PAPER_KEYWORDS values ('26', 'technology', '20','4');
+insert into PAPER_KEYWORDS values ('27', 'animal', '21','4');
 
-insert into USERS value ('1', 'Kris', 'Brown', 'username1', 
+insert into USERS value ('1', 'Kris', 'Brown', 'username1',
 'd6e08cfee902d484a95c5ccd4a34c2b60065ed1120293f8c99ac6ca2e9ed81fb',
 '3e0e71c7dcd5f79f7fddd9a5148e9fcb3973803440e940a55df2c3f526765d49',
 'k@b.edu', 'public');
 
-insert into USERS value ('2', 'Jon', 'Lee', 'username2', 
+insert into USERS value ('2', 'Jon', 'Lee', 'username2',
 'd6e08cfee902d484a95c5ccd4a34c2b60065ed1120293f8c99ac6ca2e9ed81fb',
 '3e0e71c7dcd5f79f7fddd9a5148e9fcb3973803440e940a55df2c3f526765d49',
 'j@l.edu', 'student');
 
-insert into USERS value ('3', 'Joe', 'Doe', 'username3', 
+insert into USERS value ('3', 'Joe', 'Doe', 'username3',
 'd6e08cfee902d484a95c5ccd4a34c2b60065ed1120293f8c99ac6ca2e9ed81fb',
 '3e0e71c7dcd5f79f7fddd9a5148e9fcb3973803440e940a55df2c3f526765d49',
 'j@d.edu', 'faculty');
 
-insert into USERS value ('4', 'Boss', 'Guy', 'username4', 
+insert into USERS value ('4', 'Boss', 'Guy', 'username4',
 'd6e08cfee902d484a95c5ccd4a34c2b60065ed1120293f8c99ac6ca2e9ed81fb',
 '3e0e71c7dcd5f79f7fddd9a5148e9fcb3973803440e940a55df2c3f526765d49',
 'b@g.edu', 'admin');
@@ -90,11 +90,15 @@ insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('7', '3');
 insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('8', '3');
 insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('9', '3');
 insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('10', '3');
+insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('11', '3');
+insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('12', '3');
 
-insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('10', '4');
-insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('11', '4');
-insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('12', '4');
 insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('13', '4');
 insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('14', '4');
 insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('15', '4');
 insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('16', '4');
+insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('17', '4');
+insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('18', '4');
+insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('19', '4');
+insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('20', '4');
+insert into PAPERS_USERS_MAP (papers_fk, users_fk) value ('21', '4');
