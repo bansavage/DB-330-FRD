81a53fc1bc2dfe6dbfe4a5ed9f3f67778a7c219a
Chris Dumlao <cxd3498@rit.edu>
2015-12-15 02:09:47 -0500

    Create changes

--- src/views/assets/js/create.js
+++ src/views/assets/js/create.js
@@ -4,6 +4,7 @@
 	function init(){
 	 	var submit = document.getElementById('db-add');
 	 	submit.addEventListener('click', create );
+
 	 	$("#add-auth").click(
 	 		function(){
 	 			var addBtn = $("#add-auth").clone();
@@ -22,7 +23,7 @@
 	}
 
 	function valueOf(id){
-		return $(id).value;
+		return document.getElementById(id).value;
 	}
 
 	 function create(evt){
@@ -32,32 +33,33 @@
 	 		m_token = localStorage.getItem("token");
 	 	}
 
-		var m_title     = valueOf("m-title");
-		var m_abstract  = valueOf("m-abstract");
-		var m_citations = valueOf("m-citation");
-		var m_keywords = {};
-		var m_authors = {};
-	 /*	for(var i = 0; i<10; i++){
+		// var m_title     = "a title";//valueOf("m-title");
+		// var m_abstract  = "asmnfjf";//valueOf("m-abstract");
+		// var m_citations = "choite";//valueOf("m-citation");
+		// var m_keywords  = ["1", "2"];
+		// var m_authors   = ["2", "3"];
+		var data = {
+	 			title:    "m_title",
+	 			abstract: "m_abstract",
+	 			citation: "m_citations",
+	 			keywords: [],
+	 			authors:  [],
+	 			token:    m_token
+	 		};
+	    /*for(var i = 0; i<10; i++){
 	 		m_keywords[i] = get[i].value;
 	 	}*/
 
 	 	$.ajax({
 	 		url: "/api/papers/create",
 	 		type: "POST",
-	 		data : {
-	 			title:    m_title,
-	 			abstract: m_abstract,
-	 			citation: m_citations,
-	 			keywords: m_keywords,
-	 			authors:  m_authors,
-	 			token:    m_token
-	 		},
+	 		data: data,
 	 		success : function(data, textStatus, jqXHR){
-	 			
+	 			console.log(data);
 	 			swal("Paper Created!", "Paper has been created successfully", "success")
 
 				if (status > 400){
-			        console.log("wrong username or password");
+			        console.log("Failed to create Paper");
 			        swal({
 					  title: "<h2 style='color:#DD6B55;'>Oppps!</h2>",
 					  text: "Failed to add paper",
@@ -65,20 +67,21 @@
 					  html: true
 					});
 	      }else{
-		   console.log("etaete");
-	        
+		    console.log("etaete");
+	        console.log(data);
 	      }
 	      console.log(textStatus);
 	      return true;
 	    },
 	    error : function(jqXHR, textStatus, errorThrown){
-	      console.log(textStatus);
+	      console.log("not success " + textStatus);
+	      console.log(data);
 	      swal({
 			  title: "<h2 style='color:#DD6B55;'>Oppps!</h2>",
 			  text: "Failed to add paper",
 			  imageUrl: "../assets/img/bad.png",
 			  html: true });
-	    }
+	      }
 	 	});
 	 	//evt.preventDefault();
 	 	return false;
