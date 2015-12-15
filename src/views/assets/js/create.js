931a17f976e4f3c2802b5e0b744ccce3ebc22571
Chris Dumlao <cxd3498@rit.edu>
2015-12-15 09:02:56 -0500

    Create works, can add up to 5 Keywords now

--- src/views/assets/js/create.js
+++ src/views/assets/js/create.js
@@ -1,19 +1,20 @@
 (function(){
 
+	//WEIRD STUFF GOIN ONS
 	function getAuthors(){
 		
 		var m_token ="";
 	 	if( localStorage.getItem("token") ){
 	 		m_token = localStorage.getItem("token");
 	 	}
-
 		$.ajax({
 	 		url: `/api/users/all?token=${m_token}`,
 	 		type: "GET",
+	 		async:
 	 		success : function(data, textStatus, jqXHR){
 
-	 			console.log(data);
-	 			return data;
+	 			return JSON.stringify(data);
+
 	 			
 	 			if (status > 400){
 			        console.log("Failed");
@@ -23,43 +24,76 @@
 	    });
 	}
 
+
+
 	function init(){
 
-		var authors = getAuthors();
-
-		console.log(authors);
-
 	 	var submit = document.getElementById('db-add');
+	 	var add_key = document.getElementsByClassName('add-key')[0];
 	 	submit.addEventListener('click', create );
+	 	add_key.addEventListener('click', addKey);
 
-	 	$("#add-auth").click(
-	 		function(){
-	 			var addBtn = $("#add-auth").clone();
-				//$("#add-auth").remove();
-				$("br").After(".auth-div:last-child");
-				$(".auth-div:last-child").clone().appendTo(".auth-div");
-				$(".pAuth:last-child").value="";
-				$(".pAuth:last-child").placeholder="Author";
-				console.log("auth clicked");
-	 		}
-	 	);
 	 	console.log("added Event add auth");
 	}
 
-	function newAuthInput(){
+	function genAuthors(){
 
+		var a = JSON.parse('{"users":[{"users_id":"1","fName":"Kris","lName":"Brown"},{"users_id":"2","fName":"Jon","lName":"Lee"},{"users_id":"3","fName":"Joe","lName":"Doe"},{"users_id":"4","fName":"Boss","lName":"Guy"}]}');
+		var users = a.users;
+		var numUsers = Object.keys(a.users).length;
+		var authArr = [];
+		console.log(users);
+		console.log(numUsers);
+		for(var i = 0; i<numUsers; i++){
+			authArr.push({
+				name: users[i].fName + " " + users[i].lName,
+				  id: users[i].users_id
+			}
+		);
+		//Split here into sep methods
+		
+}
+return authArr;
 	}
 
+	function addKey(evt){
+		
+		genAuthors();
+
+		
+		var num = document.getElementsByClassName("add-key").length;
+		console.log(num);
+		if( document.getElementsByClassName("pKey").length < 5){
+		$(".key-div").append("<div class='w100 flex key-cont'><input type='text' class='pure-input-1-2 pKey' name='paperKeywords' placeholder='Keyword'><i class='add-key fa fa-plus-circle pure-btn'></i></div><br/>");
+		document.getElementsByClassName('add-key')[num-1].remove();
+		document.getElementsByClassName('add-key')[num-1].addEventListener('click', addKey);
+		}
+		else{
+			swal("Sorry Buddy", "Does this paper really need that many keywords?");
+		}
+	}
+
+	/*
+	 *Gets value of element with specified id
+	 */
 	function valueOf(id){
 		return document.getElementById(id).value;
 	}
 
+	/*Creates a paper*/
 	 function create(evt){
 		
 		var m_token ="";
 	 	if( localStorage.getItem("token") ){
 	 		m_token = localStorage.getItem("token");
 	 	}
+	 	var keywords = [];
+	 	for(var k = 0; k<document.getElementsByClassName("pKey").length;k++){
+	 		var val = document.getElementsByClassName("pKey")[k].value;
+	 		console.log(val);
+	 		keywords.push( val );
+	 	}
+	 	console.log(keywords);
 
 		// var m_title     = "a title";//valueOf("m-title");
 		// var m_abstract  = "asmnfjf";//valueOf("m-abstract");
@@ -70,24 +104,26 @@
 	 			title:    valueOf("m-title"),
 	 			abstract: valueOf("m-abstract"),
 	 			citation: valueOf("m-citation"),
-	 			keywords: ["dfg", "plz", "HELLLO"],
+	 			keywords: keywords,
 	 			authors:  ["2", "3"],
 	 			token:    m_token
 	 		};
+
+	 	// Send paper ID
 	    /*for(var i = 0; i<10; i++){
 	 		m_keywords[i] = get[i].value;
 	 	}*/
 
 	 	$.ajax({
-	 		url: "/api/papers/create",
+	 		url: "/api/papers/create", // /delete //  /edit-abstract  /
 	 		type: "POST",
-	 		data: JSON.stringify(data),
+	 		data: JSON.stringify(data), //
 	 		contentType: "application/json",
 	 		success : function(data, textStatus, jqXHR){
 	 			console.log(data);
 	 			swal("Paper Created!", "Paper has been created successfully", "success")
 
-				if (status > 400){
+				if (status > 400){ //FAILED
 			        console.log("Failed to create Paper");
 			        swal({
 					  title: "<h2 style='color:#DD6B55;'>Oppps!</h2>",
