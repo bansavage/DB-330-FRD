8a54288ae438f12db7b19602cc2f9b2b15466124
lance13c <pianoman13c@gmail.com>
2015-12-16 18:41:46 -0500

    Improved navigation and error handling on all pages

--- src/views/assets/js/startPages.js
+++ src/views/assets/js/startPages.js
@@ -49,7 +49,40 @@
   // Acts just like the user clicked on the a tag, but attaches the token to the request
   var goWithToken = function(href){
     console.log(href);
-    window.location.href = `${href}?token=${token}`;
+    var token = localStorage.getItem("token");
+    //Checks to see if the next page is authorized
+    $.ajax({
+      url: `${href}`,
+      type: "GET",
+      headers: {
+        'x-access-token': token
+      },
+      success : function(data, textStatus, jqXHR){
+        if (status > 400){
+          //window.location.href = `/login`;
+          console.log(data);
+          swal({
+            title: "<h2 style='color:#DD6B55;'>This Page is Unaccessable</h2>",
+    			  text: "Quick Fixes: Try adding a paper.",
+    			  imageUrl: "../assets/img/bad.png",
+    			  html: true
+          });
+        }else{
+          window.location.href = `${href}?token=${token}`;
+        }
+        console.log(textStatus);
+        return true;
+      },
+      error : function(jqXHR, textStatus, errorThrown){
+        console.log(textStatus);
+        swal({
+  			  title: "<h2 style='color:#DD6B55;'>This Page is Unaccessable</h2>",
+  			  text: "Quick Fixe: Try adding a paper.",
+  			  imageUrl: "../assets/img/bad.png",
+  			  html: true
+        });
+      }
+    });
   }
 
   init();
